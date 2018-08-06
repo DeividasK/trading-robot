54b43058fbf64d61fbe7c2550df8c33907b6a4fd
DeividasK <deividas.karzinauskas@gmail.com>
2018-08-06 07:22:23 +0100

    SMAs

--- index.js
+++ index.js
@@ -20,27 +20,80 @@ async function getAccounts() {
   }
 }
 
-async function openPositions() {
+async function getOpenPositions() {
   try {
     const accounts = await getAccounts();
     console.log(`accounts`, accounts);
     const accountId = accounts[0].id;
-    const response = await request(`/accounts/${accountId}/positions`);
+    const response = await request(`/accounts/${accountId}/openPositions`);
     console.log(`received response`, response);
   } catch (error) {
     logError(error);
   }
 }
 
-openPositions();
+async function createOrder() {
+  try {
+    const accounts = await getAccounts();
+    console.log(`accounts`, accounts);
+    const accountId = accounts[0].id;
+    const response = await request(`/accounts/${accountId}/orders `, {
+      method: "POST",
+      body: JSON.stringify({
+        order: {
+          units: "1",
+          instrument: "EUR_GBP",
+          type: "MARKET",
+        },
+      }),
+    });
+    console.log(`received response`, response);
+  } catch (error) {
+    logError(error);
+  }
+}
 
-// async function getHistoricalData() {
-//   try {
-//     const data = await fetch();
-//     console.log(`received data`, data);
-//   } catch (error) {
-//     console.log(`error`, error);
-//   }
-// }
+async function getPricing() {
+  try {
+    const accounts = await getAccounts();
+    console.log(`accounts`, accounts);
+    const accountId = accounts[0].id;
+    const response = await request(
+      `/accounts/${accountId}/pricing?instruments=EUR_GBP `,
+    );
+    console.log(`received response`, JSON.stringify(response));
+  } catch (error) {
+    logError(error);
+  }
+}
 
-// getHistoricalData();
+async function getCandles(instrument, options) {
+  try {
+    const query = Object.keys(options)
+      .map((filter, index) => {
+        const join = index === 0 ? "?" : "&";
+        return `${join}${filter}=${options[filter]}`;
+      })
+      .join("");
+
+    const response = await request(
+      `/instruments/${instrument}/candles${query}`,
+    );
+    // console.log(`received response`, JSON.stringify(response));
+    const sma = response.candles.reduce((acc, candle) => {
+      if (!candle.complete) {
+        return acc;
+      }
+      return acc + new Number(candle.mid.c) / options.count;
+    }, 0);
+    console.log(`${options.count} ${options.granularity} SMA`, sma.toFixed(4));
+  } catch (error) {
+    logError(error);
+  }
+}
+
+// getOpenPositions();
+// createOrder();
+// getPricing();
+getCandles("EUR_GBP", { count: 90, granularity: "D" });
+getCandles("EUR_GBP", { count: 7, granularity: "D" });
--- request.js
+++ request.js
@@ -8,15 +8,17 @@ function checkStatus(response) {
   if (response.status > 199 && response.status < 300) {
     return response.json();
   }
+  console.log(`response`, response);
 
   throw new Error(
-    `Response status ${response.status}. Response ${JSON.stringify(response)}.`,
+    `Response status ${response.status}. Error ${response.statusText}.`,
   );
 }
 
 export async function request(endpoint, options) {
   const defaultOptions = {
     headers: {
+      "Content-Type": "application/json",
       Authorization: `Bearer ${API_KEY}`,
     },
   };
